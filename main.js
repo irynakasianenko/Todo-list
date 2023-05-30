8514370284fdc57e5bb4a77ee869922ea1665086
Iryna Kasianenko <iryna.kasianenko98@gmail.com>
2023-05-30 12:06:02 +0300

    add class Filter

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let currentDate = document.querySelector('#current-date');
 let input = document.querySelector('#input');
 let date = document.querySelector('input[type = "date"]');
 let important = document.querySelector('#important');
@@ -17,7 +18,13 @@ input.addEventListener("keydown", function (e) {
 	if (e.code == "Enter") addTask();
 });
 
-expireDate.addEventListener('click', show);
+expireDate.addEventListener('click', function (e) {
+	let filter = new Filter(e.target, taskList);
+	filter.show();
+});
+
+let dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+currentDate.textContent = new Date().toLocaleString('en-US', dateOptions);
 
 function addTask() {
 	if (input.value) {
@@ -29,60 +36,6 @@ function addTask() {
 	} else {
 		alert("введіть ім'я завдання");
 	}
-}
-
-function show(e) {
-	switch (e.target) {
-		case all:
-			showAll();
-			break;
-		case today:
-			showAll();
-			break;
-		case overdue:
-			showAll();
-			break;
-		case completed:
-			showCompleted();
-			break;
-		case uncompleted:
-			showNotCompleted();
-			break;
-	}
-}
-
-function showAll() {
-	taskList.innerHTML = "";
-	tasks.forEach(task => {
-		task.createIn(taskList);
-	});
-}
-
-function showTodayTasks() {
-	taskList.innerHTML = "";
-	tasks
-		.filter(task => task.date == false)
-		.forEach(task => {
-			task.createIn(taskList);
-		});
-}
-
-function showCompleted() {
-	taskList.innerHTML = "";
-	tasks
-		.filter(task => task.isDone == true)
-		.forEach(task => {
-			task.createIn(taskList);
-		});
-}
-
-function showNotCompleted() {
-	taskList.innerHTML = "";
-	tasks
-		.filter(task => task.isDone == false)
-		.forEach(task => {
-			task.createIn(taskList);
-		});
 }
 
 class toDoList {
@@ -141,20 +94,110 @@ class toDoList {
 	}
 }
 
-let todayDate = function () {
-	let date = new Date();
-	let dateArray = [];
-	dateArray.push(date.getFullYear());
-	let month = function () {
-		a = date.getMonth() + 1;
-		if (a < 10) {
-			a = 0 + String(month);
-		}
-		return a;
+class Filter {
+	constructor(element, elementCreateIn) {
+		this.element = element;
+		this.elementCreateIn = elementCreateIn;
 	}
-	dateArray.push(month());
-	dateArray.push(date.getDate());
-	return dateArray.join('-');
+
+	show() {
+		switch (this.element) {
+			case all:
+				this.showAll();
+				break;
+			case today:
+				this.showToday();
+				break;
+			case overdue:
+				this.showOverdue();
+				break;
+			case completed:
+				this.showCompleted();
+				break;
+			case uncompleted:
+				this.showNotCompleted();
+				break;
+		}
+	}
+
+	showAll() {
+		this.elementCreateIn.innerHTML = "";
+		tasks.forEach(task => {
+			task.createIn(this.elementCreateIn);
+		});
+	}
+
+	showCompleted() {
+		this.elementCreateIn.innerHTML = "";
+		tasks
+			.filter(task => task.isDone == true)
+			.forEach(task => {
+				task.createIn(this.elementCreateIn);
+			});
+	}
+
+	showNotCompleted() {
+		this.elementCreateIn.innerHTML = "";
+		tasks
+			.filter(task => task.isDone == false)
+			.forEach(task => {
+				task.createIn(this.elementCreateIn);
+			});
+	}
+
+	showToday() {
+		this.elementCreateIn.innerHTML = "";
+		tasks
+			.filter(task => this.compareDate(task.date) == 'today')
+			.forEach(task => {
+				task.createIn(this.elementCreateIn);
+			});
+	}
+
+	showOverdue() {
+		this.elementCreateIn.innerHTML = "";
+		tasks
+			.filter(task => this.compareDate(task.date) == 'overdue')
+			.forEach(task => {
+				task.createIn(this.elementCreateIn);
+			});
+	}
+
+	compareDate(value) {
+		let array = value.split('-');
+		let year = array[0];
+		let month = array[1];
+		let day = array[2];
+		let date = new Date();
+		let currentYear = date.getFullYear();
+		let currentMonth = () => {
+			if (date.getMonth() < 10) {
+				return '0' + String(date.getMonth() + 1)
+			} else {
+				return date.getMonth() + 1;
+			}
+		}
+		let currentDay = () => {
+			if (date.getDate() < 10) {
+				return '0' + String(date.getDate() + 1)
+			} else {
+				return date.getDate();
+			}
+		}
+
+		if (year == currentYear) {
+			if (month == currentMonth()) {
+				if (day == currentDay()) {
+					return 'today';
+				} else if (day < currentDay()) {
+					return 'overdue';
+				}
+			} else if (month < currentMonth()) {
+				return 'overdue';
+			}
+		} else if (year < currentYear()) {
+			return 'overdue';
+		}
+	}
 }
 
-console.log(todayDate())
